2361bacb67c08aad356fd2a6a4edfd7fcf990bab
CompileWithG <codewithkarthikg@gmail.com>
2024-08-24 00:10:50 +0530

    Previosly empty @layers/auth/composables/useAuth.ts file now holds the code that maps the data returned from the authentication provider to the correct adapter ,after that data is converted to the userType format the useDB function is called to enter the data into the database

---
+++ layers/auth/composables/useAuth.ts
@@ -0,0 +1,26 @@
+// Example usage for Google Auth
+
+import { googleAdapter } from '@/layers/auth/providers/oauth/google';
+import { useDb } from '@/layers/storage/databases/sql/useDb';
+import { githubAdapter } from '@/layers/auth/providers/oauth/github';
+import { metamaskAdapter } from '@/layers/auth/providers/oauth/metamask';
+async function handleAuthCallback(authData: any, provider: string) {
+  let user;
+  
+  switch (provider) {
+    case 'google':
+      user = googleAdapter(authData);
+      break;
+    case 'github':
+      user = githubAdapter(authData);
+      break;
+    case 'metamask':
+      user = metamaskAdapter(authData);
+      break;
+    default:
+      throw new Error('Unsupported provider');
+  }
+  
+  // Store the user data in the database
+  await useDb(user.username,user.useremail,user.userid);
+}
